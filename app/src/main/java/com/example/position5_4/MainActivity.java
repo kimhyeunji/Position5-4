bebca072bfb4bdb2f9b74989ce7914b52ffa92a8
Kimhyeunji <guswl78978@naver.com>
2023-05-09 14:36:17 +0900

    Initial commit(p.221) 직접풀어보기 5-4

---
+++ app/src/main/java/com/example/position5_4/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.position5_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"전화번호를 입력합니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
